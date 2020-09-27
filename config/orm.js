da75ebab64aaa20ccd57e2df6e9f3ded99c9492d
Jeneth Diesta <jen6one9@gmail.com>
2020-09-27 05:21:56 -0700

    setup ORM function bones

---
+++ config/orm.js
@@ -0,0 +1,48 @@
+var connection = require("./connection.js");
+
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
+var orm = {
+  selectAll: function(whatToSelect, table, orderCol) {
+    var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
+    console.log(queryString);
+    connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+ insertOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+    var queryString =
+      "SELECT ??, INSERT(??) FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
+
+    connection.query(
+      queryString,
+      [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
+      function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      }
+    );
+  }
+};
+
+One: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+    var queryString =
+      "SELECT ??, INSERT(??) FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
+
+    connection.query(
+      queryString,
+      [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
+      function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      }
+    );
+  }
+};
+
+module.exports = orm;
