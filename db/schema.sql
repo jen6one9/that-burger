684d95d0ce3c745d0d51a4e43ec2ef1f60df0a11
Jeneth Diesta <jen6one9@gmail.com>
2020-09-27 04:32:37 -0700

    updated boolean

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,13 @@
-DROP DATABASE IF EXISTS burgers_db;
+### Schema
 
-
--- Create the database  
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
--- Create the table  
-CREATE TABLE burgers;
+CREATE TABLE burgers
 (
-  id int NOT NULL
-  AUTO_INCREMENT,
-  burger_name varchar
-  (30) NOT NULL,
-  devoured varchar
-  (255) NOT NULL,
-  completed BOOLEAN
-  
-  PRIMARY KEY
-  (id)
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(35) NOT NULL,
+    devoured BOOLEAN,
+   	PRIMARY KEY (id)
 );
