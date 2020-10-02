5df5feec669500ad3e2510e468cd3eef9e664b22
Jeneth Diesta <jen6one9@gmail.com>
2020-10-01 17:16:28 -0700

    cleaned up several edits in the controller file, models, and added script js file

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,46 +1,61 @@
-var router = require("express").Router()
-var db = require("../db/db.json")
-var fs = require("fs")
-const { maxHeaderSize } = require("http")
+var express = require("express");
 
+var router = express.Router();
 
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
 
-router = require("express").Router();
-router.get("/api/burger_name", function (req, res) { //Should read the db file and return all hamburgers
-    db = JSON.parse(fs.readfilesync("./db.json", "utf8"))  //utf8 means reads in english format
-    console.log("get route", db)
-    res.json(db)
-})
-router.post("/api/burger_name", function (req, res) { //Should receive a new hamburger to save on the request body, add it to the db.json file, and then return the new note to the client.
-    var data = {
-        id: Math.floor(Math.random() * 100),
-        title: req.body.title,
-        text: req.body.text
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne([
+    "burger_name", "devoured"
+  ], [
+    req.body.name, false
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.true
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
     }
-    db.push(data)
-    fs.writeFileSync("../db/db.json", JSON.stringify(db), function () {
-        console.log("File Updated")
-        console.log("post route", db)
-        res.json(db)
-    })
+  });
+});
 
-})
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
-router.delete("/api/devour/:id", function (req, res) { //Should receive a query parameter containing the id of a burger to devour. 
-    db = JSON.parse(fs.readfilesync("./db.json", "utf8"))  //utf8 means reads in english format
-    console.log("delete route", db)
-    res.json(db)
-    fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(db), function (err) {
-        if (err) console.log(err);
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
-        console.log("delete note");
-    })
-
-    console.log(notes);
-    res.send("DELETE Request Called")
-})
-
-module.exports = router //lowercase r for the var you create
-
-//this page is where we manipulate the data (data-handling)
-
+// Export routes for server.js to use.
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -19,7 +19,12 @@ var burger = {
         orm.updateOne("burgers", objColVals, condition, function(res){
             cb(res);
         });
-    }
+    },
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+          cb(res);
+        });
+      }
     };
 
 // Export the database functions for the controller (burgers_controller.js).
